6171cda6fe8ead48afe00e50c9bf043ebcdd4290
jinghong <jinghong0202@foxmail.com>
2023-10-19 18:55:15 +0800

    docs(components): update types

--- types/utils/index.d.ts
+++ types/utils/index.d.ts
@@ -1,6 +1,5 @@
 export * from './vue/directives/resize';
 export * from './runtime/common';
 export * from './runtime/indexedDBUtils';
-export * from './runtime/perttier';
 export * from './runtime/map/ol/animation';
 export * from './runtime/canvas/rotation';
